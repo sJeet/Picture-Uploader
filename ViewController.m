3367caf6c42d6959f9b70670ea1da63a86fe1a08
sJeet <jeet.malay.shah@gmail.com>
2014-04-24 15:21:29 -0700

    Update ViewController.m

--- ViewController.m
+++ ViewController.m
@@ -38,27 +38,30 @@
 }
 
 - (IBAction)uploadImage:(id)sender {
-    NSData *imageData = UIImageJPEGRepresentation(chosenImage,90);
-//    NSString *uploadUrl = @"http://jeetshah.com/PHPJavaServer.php?";
-    NSString *uploadUrl = @"http://davesagar.com/PhpServerSide.php?";
     
-    NSMutableURLRequest * request = [[NSMutableURLRequest alloc] init];
-    [request setURL:[NSURL URLWithString:uploadUrl]];
-    [request setHTTPMethod:@"POST"];
-    
-    NSString *boundary = @"---------------------------14737809831466499882746641449";
-    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary];
-    [request addValue:contentType forHTTPHeaderField: @"Content-Type"];
-    
-    NSMutableData *body = [NSMutableData data];
-    [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:[[NSString stringWithString:[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"userfile\"; filename=\"%d\"\r\n", 1]] dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:[@"Content-Type: application/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:[NSData dataWithData:imageData]];
-    [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    [request setHTTPBody:body];
-    
-    [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+ NSData *imageData = UIImageJPEGRepresentation(imageView.image, 90);
+ NSString *urlString = @"http://jeetshah.com/serverPHP.php";
+ 
+ NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+ [request setURL:[NSURL URLWithString:urlString]];
+ [request setHTTPMethod:@"POST"];
+ 
+ NSString *boundary = @"---------------------------14737809831466499882746641449";
+ NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary];
+ [request addValue:contentType forHTTPHeaderField:@"Content-Type"];
+ 
+ NSMutableData *body = [NSMutableData data];
+ [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+ [body appendData:[@"Content-Disposition: form-data; name=\"userfile\"; filename=\"file.jpg\"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+ [body appendData:[@"Content-Type: application/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+ [body appendData:[NSData dataWithData:imageData]];
+ [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+ [request setHTTPBody:body];
+ 
+ NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+ NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+ 
+ NSLog(@"%@",returnString);
 
 }
 
