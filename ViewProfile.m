c75906062f0f57c45f84fc1714f9b31f1af81452
sJeet <jeet.malay.shah@gmail.com>
2014-06-06 23:25:51 -0700

    Update ViewProfile.m

--- ViewProfile.m
+++ ViewProfile.m
@@ -29,12 +29,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [profile setImage:[UIImage imageNamed:@"unknown.png"] forState:UIControlStateNormal];
+    [profile setImage:[UIImage imageNamed:@"icon.png"] forState:UIControlStateNormal];
     profile.frame = CGRectMake(135.0, 180.0, 200, 200);//width and height should be same value
     profile.clipsToBounds = YES;
     
     profile.layer.cornerRadius = 100;//half of the width
-    profile.layer.borderColor=[UIColor blackColor].CGColor;
+    profile.layer.borderColor=[UIColor whiteColor].CGColor;
     profile.layer.borderWidth=2.0f;
     
     [self.view addSubview:profile];
@@ -47,7 +47,7 @@
     
     // Create the NSURLConnection
     [NSURLConnection connectionWithRequest:urlRequest delegate:self];
-        // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning
@@ -66,18 +66,19 @@
 {
     // Append the newly downloaded data
     [_downloadedData appendData:data];
-        
+    
 }
 
 - (void)connectionDidFinishLoading:(NSURLConnection *)connection
 {
-    firstName.text = @"Jeet";
-    lastName.text = @"Shah";
+    //JSON Parsing data - Yet to done
+    //firstName.text = @"Jeet";
+    //lastName.text = @"Shah";
     
 }
 
 - (IBAction)msg:(id)sender {
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"TEst" message:nil delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Hi this is Jeet Shah" message:nil delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
     
     [alert show];
 }
